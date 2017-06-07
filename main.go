8bb670e0ad98f8919b5c1ec92238236333e5d611
WAKAYAMA Shirou <shirou.faw@gmail.com>
2017-06-07 13:02:45 +0900

    download more images

--- main.go
+++ main.go
@@ -16,21 +16,27 @@ import (
 	"github.com/raff/godet"
 )
 
-func findNewSrc(root string, attrs []string) string {
+func findNewSrc(root string, attrs []string) (string, string, string) {
 	for i, attr := range attrs {
 		if attr != "href" && attr != "src" {
 			continue
 		}
-		url, err := url.Parse(attrs[i+1])
+		n := attrs[i+1]
+
+		// skip if data attribute
+		if strings.Contains(n, "data") {
+			return attr, n, n
+		}
+		url, err := url.Parse(n)
 		if err != nil {
-			log.Fatalf("url parse error: %v", attrs[i+1])
+			log.Fatalf("url parse error: %v", n)
 		}
 
 		fname := filepath.Base(url.Path)
-		return filepath.Join(root, fname)
+		return attr, n, filepath.Join(root, fname)
 	}
 
-	return ""
+	return "", "", ""
 }
 
 func GetTitle(remote *godet.RemoteDebugger) (string, error) {
@@ -85,15 +91,18 @@ func GetAttributes(remote *godet.RemoteDebugger, nodeId int) ([]string, error) {
 	return ret, nil
 }
 
-func replace(remote *godet.RemoteDebugger, root, query string) error {
+func replace(remote *godet.RemoteDebugger, root, query string) ([]string, error) {
+	var orig []string
+
 	res, err := remote.QuerySelectorAll(1, query)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if res == nil {
 		log.Fatalf("not err")
-		return nil
+		return orig, nil
 	}
+
 	for _, i := range res["nodeIds"].([]interface{}) {
 		nodeId, ok := i.(float64)
 		if !ok {
@@ -101,18 +110,23 @@ func replace(remote *godet.RemoteDebugger, root, query string) error {
 		}
 		node, err := GetAttributes(remote, int(nodeId))
 		if err != nil {
-			return err
+			return nil, err
 		}
-		f := findNewSrc(root, node)
+		attr, original, f := findNewSrc(root, node)
+		orig = append(orig, original)
 
-		if err := remote.SetAttributeValue(int(nodeId), "src", f); err != nil {
-			return err
+		if err := remote.SetAttributeValue(int(nodeId), attr, f); err != nil {
+			return nil, err
 		}
 	}
-	return nil
+	return orig, nil
 }
 
 func download(reqURI, root string) error {
+	if strings.HasPrefix(reqURI, "//") {
+		reqURI = "http:" + reqURI
+	}
+
 	u, err := url.Parse(reqURI)
 	if err != nil {
 		return err
@@ -156,6 +170,25 @@ func EnsureDirectory(path string) {
 	}
 }
 
+func mergeURLs(orig, new []string) []string {
+	appending := make([]string, 0)
+
+	for _, n := range new {
+		var found bool
+		for _, o := range orig {
+			if n == o {
+				found = true
+			}
+		}
+		if !found {
+			appending = append(appending, n)
+		}
+	}
+
+	return append(orig, appending...)
+
+}
+
 func main() {
 	flag.Parse()
 	URI := flag.Arg(0)
@@ -173,13 +206,14 @@ func main() {
 	var urls []string
 
 	remote.CallbackEvent("Network.requestWillBeSent", func(params godet.Params) {
+		fmt.Println(params)
 		req, ok := params["request"].(map[string]interface{})
 		if !ok {
 			return
 		}
 
 		url, ok := req["url"].(string)
-		if ok && willDownload(url) {
+		if ok { // && willDownload(url) {
 			urls = append(urls, url)
 		}
 	})
@@ -189,22 +223,29 @@ func main() {
 
 		EnsureDirectory(root)
 
-		for _, url := range urls {
-			if err := download(url, root); err != nil {
-				log.Fatalf("download failed: %v", err)
-			}
-		}
-
 		_, err := remote.GetDocument() // なぜか必要
 		if err != nil {
 			log.Fatalf("GetDocumentFailed: %v", err)
 		}
-
-		if err := replace(remote, root, "[src]"); err != nil {
-			log.Fatalf("replace : %v", err)
+		{
+			new, err := replace(remote, root, "[src]")
+			if err != nil {
+				log.Fatalf("replace : %v", err)
+			}
+			urls = mergeURLs(urls, new)
 		}
-		if err := replace(remote, root, "[href]"); err != nil {
-			log.Fatalf("replace : %v", err)
+		{
+			new, err := replace(remote, root, "[href]")
+			if err != nil {
+				log.Fatalf("replace : %v", err)
+			}
+			urls = mergeURLs(urls, new)
+		}
+
+		for _, url := range urls {
+			if err := download(url, root); err != nil {
+				log.Printf("download failed: %s, %v", url, err)
+			}
 		}
 
 		if err := writeBody(remote, root+".html"); err != nil {
